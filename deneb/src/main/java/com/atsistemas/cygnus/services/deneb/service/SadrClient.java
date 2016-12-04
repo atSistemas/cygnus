1215d6a7a168e7fd9b574666d825e3964616bb53
dani <dani@kronos>
2016-12-04 17:36:25 +0100

    adding @HystrixCommand

--- deneb/src/main/java/com/atsistemas/cygnus/services/deneb/service/SadrClient.java
+++ deneb/src/main/java/com/atsistemas/cygnus/services/deneb/service/SadrClient.java
@@ -8,24 +8,29 @@ import org.springframework.web.client.RestTemplate;
 
 import com.atsistemas.cygnus.model.PingRequest;
 import com.atsistemas.cygnus.model.PingResponse;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
 public class SadrClient {
 
 	private static Logger logger = LoggerFactory.getLogger(SadrClient.class);
 	
-	public static final String SADR_URL = "http://SADR-SERVICE/sadr/ping";
+	public static final String SADR_URL = "http://sadr-service/sadr/ping";
 	
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@HystrixCommand(fallbackMethod="retrieveFallbackPingSadr")
 	public PingResponse pingSadr(PingRequest pingRequest){
 		
-		logger.debug("--> pinging Sadr...");
-		logger.debug("--> Id: {}",pingRequest.getId());
-		logger.debug("--> Message: {}",pingRequest.getMessage());
-		
+		logger.debug("--> ping received - id: {} - content: {}", pingRequest.getId(), pingRequest.getMessage());
+
 		return restTemplate.postForObject(SADR_URL, pingRequest, PingResponse.class);
+	}
+	
+	public PingResponse retrieveFallbackPingSadr(PingRequest pingRequest){
+		
+		return new PingResponse("Error pinging sadr. This is a fallback message");
 	}
 	
 }

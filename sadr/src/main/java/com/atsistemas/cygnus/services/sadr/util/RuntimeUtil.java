d5a72e768637cf8e1c560ab889476976f11dd3f1
dani <dani@kronos>
2016-12-06 22:22:56 +0100

    runtime util added

---
+++ sadr/src/main/java/com/atsistemas/cygnus/services/sadr/util/RuntimeUtil.java
@@ -0,0 +1,25 @@
+package com.atsistemas.cygnus.services.sadr.util;
+
+import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RuntimeUtil implements ApplicationListener<EmbeddedServletContainerInitializedEvent>{
+
+	private static int port;
+	
+	@Override
+	public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
+		
+		port = event.getEmbeddedServletContainer().getPort();
+	}
+	
+	public static int getPort(){
+		
+		return port;
+	}
+	
+	
+	
+}

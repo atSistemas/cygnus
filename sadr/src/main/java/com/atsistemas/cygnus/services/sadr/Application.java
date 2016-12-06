db366d65a077a30fb0ac1f70d5b68795608657a6
dani <dani@kronos>
2016-12-06 22:21:20 +0100

    formating text

--- sadr/src/main/java/com/atsistemas/cygnus/services/sadr/Application.java
+++ sadr/src/main/java/com/atsistemas/cygnus/services/sadr/Application.java
@@ -4,13 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
 	}
-	
 }

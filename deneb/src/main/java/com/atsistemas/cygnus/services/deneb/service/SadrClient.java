0596912db04cc28c80f2d777eb2f0538350390d1
dani <dani.sb@gmail.com>
2016-12-22 00:26:16 +0100

    external config added

--- deneb/src/main/java/com/atsistemas/cygnus/services/deneb/service/SadrClient.java
+++ deneb/src/main/java/com/atsistemas/cygnus/services/deneb/service/SadrClient.java
@@ -3,19 +3,19 @@ package com.atsistemas.cygnus.services.deneb.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.client.RestTemplate;
 
 import com.atsistemas.cygnus.model.PingRequest;
 import com.atsistemas.cygnus.model.PingResponse;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
-@Service
+@ConfigurationProperties(prefix="cygnus.sadr")
 public class SadrClient {
 
 	private static Logger logger = LoggerFactory.getLogger(SadrClient.class);
 	
-	public static final String SADR_URL = "http://SADR-SERVICE/sadr/ping/";
+	private String pingUrl;
 	
 	@Autowired
 	private RestTemplate restTemplate;
@@ -24,13 +24,15 @@ public class SadrClient {
 	public PingResponse pingSadr(PingRequest pingRequest){
 		
 		logger.debug("--> pingSadr received - id: {} - content: {}", pingRequest.getId(), pingRequest.getMessage());
-
-		return restTemplate.postForObject(SADR_URL, pingRequest, PingResponse.class);
+		logger.debug("--> sadr endpoint: {}",pingUrl);
+		return restTemplate.postForObject(pingUrl, pingRequest, PingResponse.class);
 	}
 	
 	public PingResponse retrieveFallbackPingSadr(PingRequest pingRequest){
-		
 		return new PingResponse("Error pinging sadr. This is a fallback message");
 	}
-	
+
+	public void setPingUrl(String pingUrl) {
+		this.pingUrl = pingUrl;
+	}
 }

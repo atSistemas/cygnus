1137172d5b815a75aca0e29b23bc1448aaba14a4
dani <dani.sb@gmail.com>
2017-02-16 07:23:04 +0100

    SPRING_PROFILES & Turbine added

--- hystrix-dashboard/src/main/java/com/atsistemas/cygnus/hystrixdashboard/Application.java
+++ hystrix-dashboard/src/main/java/com/atsistemas/cygnus/hystrixdashboard/Application.java
@@ -3,10 +3,12 @@ package com.atsistemas.cygnus.hystrixdashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableTurbine
 public class Application {
 
 	public static void main(String[] args) {

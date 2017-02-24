bd6ab6221b46831caf6752e625b6fc6b29906cdc
dani <dani@dani-ThinkPad-E550>
2017-02-24 13:28:56 +0100

    turbine project added

---
+++ turbine/src/main/java/com/atsistemas/cygnus/turbine/Application.java
@@ -0,0 +1,16 @@
+package com.atsistemas.cygnus.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+
+@SpringBootApplication
+@EnableTurbineStream
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+	}
+	
+}

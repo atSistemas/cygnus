62d1eeb5c58b89220c2c676fc5b03bd63df4fe59
dani <dani@dani-ThinkPad-E550>
2017-02-27 17:00:14 +0100

    json pretty print added

--- turbine/src/main/java/com/atsistemas/cygnus/turbine/Application.java
+++ turbine/src/main/java/com/atsistemas/cygnus/turbine/Application.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 public class Application {
 
 	public static void main(String[] args) {
-
+		System.setProperty("spring.jackson.serialization.INDENT_OUTPUT", "true");
 		SpringApplication.run(Application.class, args);
 	}
 	

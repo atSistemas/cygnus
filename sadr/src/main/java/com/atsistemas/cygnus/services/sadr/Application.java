2e7b084d4e795b71d38d9858c8699e54faf6f4aa
dani <dani@dani-ThinkPad-E550>
2017-02-27 16:57:20 +0100

    EnableCircuitBreaker

--- sadr/src/main/java/com/atsistemas/cygnus/services/sadr/Application.java
+++ sadr/src/main/java/com/atsistemas/cygnus/services/sadr/Application.java
@@ -2,10 +2,12 @@ package com.atsistemas.cygnus.services.sadr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class Application {
 
 	public static void main(String[] args) {

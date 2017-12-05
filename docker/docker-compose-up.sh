04325c7c591a80aa3ec3e00dc3f028302cc7669a
Unknown <dani.sb@gmail.com>
2017-12-05 16:51:27 +0100

    SPRING_PROFILES_ACTIVE:local added

--- docker/docker-compose-up.sh
+++ docker/docker-compose-up.sh
@@ -3,7 +3,8 @@ export SPRING_PROFILES_ACTIVE=docker
 
 # config-server
 export CONFIG_SERVER_PORT=8888
-export CONFIG_SERVER_REPO=$HOME/git/cygnus-config
+export CONFIG_REPO=https://github.com/atSistemas/cygnus-config
+export LOCAL_CONFIG_REPO_PATH=$HOME/git/cygnus-config
 export CONFIG_SERVER_ENDPOINT=http://config-server:$CONFIG_SERVER_PORT
 
 # config retry
@@ -22,9 +23,11 @@ export ZUUL_PORT=8080
 export ADMIN_SERVER_PORT=9999
 
 # hystrix-dashboard
-export HYSTRIX_DASHBOARD_PORT=9001
+export HYSTRIX_DASHBOARD_PORT=9000
 
 # turbine
 export TURBINE_PORT=8989
 
-docker-compose up
+#docker-compose up
+
+docker-compose -f docker-compose-config.yml up

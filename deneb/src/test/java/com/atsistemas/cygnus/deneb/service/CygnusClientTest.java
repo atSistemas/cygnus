0f44da263dc62e8813967b42297f68d248e31cda
Unknown <dani.sb@gmail.com>
2017-12-11 17:59:42 +0100

    spring-boot-starter-test added

---
+++ deneb/src/test/java/com/atsistemas/cygnus/deneb/service/CygnusClientTest.java
@@ -0,0 +1,14 @@
+package com.atsistemas.cygnus.deneb.service;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+class CygnusClientTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

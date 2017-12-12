9316b9bbc1526d87cb5e1236840f6538d9bb9109
Unknown <dani.sb@gmail.com>
2017-12-12 12:18:30 +0100

    turbine testing

--- turbine/src/main/java/com/atsistemas/cygnus/turbine/Application.java
+++ turbine/src/main/java/com/atsistemas/cygnus/turbine/Application.java
@@ -3,11 +3,13 @@ package com.atsistemas.cygnus.turbine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+//import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
-@EnableTurbineStream
-@EnableDiscoveryClient
+//@EnableTurbineStream
+//@EnableDiscoveryClient
+@EnableTurbine
 public class Application {
 
 	public static void main(String[] args) {

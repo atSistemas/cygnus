18ced5d5a6a20f0a5670f97e348a3d6f01fb5019
Unknown <dani.sb@gmail.com>
2017-12-12 01:45:41 +0100

    @EnableDiscoveryClient added

--- turbine/src/main/java/com/atsistemas/cygnus/turbine/Application.java
+++ turbine/src/main/java/com/atsistemas/cygnus/turbine/Application.java
@@ -2,10 +2,12 @@ package com.atsistemas.cygnus.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
 @EnableTurbineStream
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
